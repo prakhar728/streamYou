a04d3fe2fd2c75944f5ea7850b644e17098c9261
Suhel Kapadia <suhelkapadia2@gmail.com>
2023-04-24 23:14:45 +0530

    feat: contracts added

---
+++ contracts/helper-hardhat-config.js
@@ -0,0 +1,23 @@
+const { ethers } = require("hardhat")
+
+const networkConfig = {
+    31415926: {
+	name: "Localnet",
+	tokensToBeMinted: 12000,
+    },
+    3141: {
+        name: "Hyperspace",
+        tokensToBeMinted: 12000,
+    },
+    314: {
+        name: "FilecoinMainnet",
+        tokensToBeMinted: 12000,
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+}
