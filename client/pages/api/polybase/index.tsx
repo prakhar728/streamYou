bb3717e25acbbeb23f801e8fd1538c888b14e3cb
Suhel Kapadia <suhelkapadia2@gmail.com>
2023-04-30 14:30:29 +0530

    fix: working on cors

--- client/pages/api/polybase/index.tsx
+++ client/pages/api/polybase/index.tsx
@@ -116,6 +116,9 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
+    res.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token")
     const db = signInPolybase();
 
     // Create a collection
